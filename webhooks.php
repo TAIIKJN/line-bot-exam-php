4f63df75fd40219a2f0f33e76c9715fef6a04f2a
TAIIKJN <54341640+TAIIKJN@users.noreply.github.com>
2020-04-30 19:45:51 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '43/Y1guAuNfbiAt61jcRF4rKD/Nre5ju4+XhaEN6RAhwI4pVCMrt21e3pGz8lbAhVV0pPESlH9Kkfcwp8SRNM/aYqbgtJ+rrXFzj0gIPygd+FJAF70sDdhqK/gmwy2xb73Gb5bL20TbFW39Ot9wBywdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
